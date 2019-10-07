c80d49ad014ca4001d0ddfec12bf32cb90e4350e
felipepsaraiva <felipepiressaraiva@gmail.com>
2019-10-07 16:55:46 -0300

    Implementando a mensagem de status do controle e melhorando a açao de reset

--- humanoid_control/nodes/control_node.py
+++ humanoid_control/nodes/control_node.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 import numpy as np
 import rospy
-from std_msgs.msg import Float32MultiArray, Int16MultiArray, String
+from std_msgs.msg import Float32MultiArray, Int16MultiArray, String, UInt8MultiArray
 import threading
 
 from humanoid_control.control import Control
 from humanoid_control.utils import rad_to_deg
+
+STATUS_RATE_DIVIDER = 24
+DIVIDER_COUNTER = 0
+
 
 def publish_joint_pos():
   data = rad_to_deg(control.angulos[:]).astype(np.int16)
@@ -13,11 +17,17 @@ def publish_joint_pos():
   msg.data = data
   joint_pub.publish(msg)
 
+
+def publish_control_status():
+  msg = UInt8MultiArray()
+  msg.data = [control.state_encoder[control.state], control.manual_mode]
+  status_pub.publish(msg)
+
+
 def command_callback(msg):
   cmd = msg.data
-  rospy.loginfo('Received command: ' + cmd)
   if cmd == 'reset':
-    control.reset()
+    rospy.signal_shutdown('reset')
   elif cmd == 'walk':
     if control.manual_mode:
       control.visao_bola = True
@@ -28,14 +38,16 @@ def command_callback(msg):
   elif cmd == 'set_mode_auto':
     control.manual_mode = False
 
+
 if __name__ == "__main__":
   rospy.init_node('control_node')
   rospy.loginfo('Pequi Mecanico Humanoid - Control Node')
-  rate = rospy.Rate(120)
+  rate = rospy.Rate(110)
 
-  control = Control(gravity_compensation_enable=False)
+  control = Control(gravity_compensation_enable=True)
 
   joint_pub = rospy.Publisher('/PMH/joint_pos', Int16MultiArray, queue_size=1)
+  status_pub = rospy.Publisher('/PMH/control_status', UInt8MultiArray, queue_size=1)
   rospy.Subscriber('PMH/vision_status', Float32MultiArray, control.vision_status_callback)
   rospy.Subscriber('PMH/control_command', String, command_callback)
 
@@ -46,8 +58,13 @@ if __name__ == "__main__":
 
   try:
     while not rospy.is_shutdown():
-      publish_joint_pos()
       rate.sleep()
+      publish_joint_pos()
+
+      DIVIDER_COUNTER += 1
+      if DIVIDER_COUNTER >= STATUS_RATE_DIVIDER:
+        publish_control_status()
+        DIVIDER_COUNTER = 0
   except KeyboardInterrupt:
     pass
 
